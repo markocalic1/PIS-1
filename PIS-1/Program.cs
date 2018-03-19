e1f0208e86169775623c80e5daf20c4103c9938c
3351drago <dragobarisic@hotmail.com>
2018-03-19 12:01:47 +0100

    Update Program.cs

--- PIS-1/Program.cs
+++ PIS-1/Program.cs
@@ -9,7 +9,8 @@ namespace PIS_1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
+            Console.WriteLine("Hello World"); // ispis Hello World
         }
     }
 }
+// Dodan komentar iza linije koda
